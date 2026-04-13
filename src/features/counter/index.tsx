331b426eff778e09ca63a11058bf39496ef6427a
vietdan1613 <viet.dan.1613@gmail.com>
2026-04-13 12:07:14 +0700

    update counter

--- src/features/counter/index.tsx
+++ src/features/counter/index.tsx
@@ -1,7 +1,14 @@
+// Build a Counter app using React (TypeScript) that supports
+// - increment
+// - decrement(not below 0)
+// - reset 
+// - increasing by a custom numeric value from an input field.
+// - Ensure invalid input is handled properly and structure your state in a scalable way.
+
 export default function App() {
 	return (
-		<div>
-			Counter
-		</div>
+		<div className="flex flex-col">
+			Start here
+		</div >
 	)
 }
