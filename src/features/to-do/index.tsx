67f096d36f20ba12feb18492b495a731cf3f3f43
vietdan1613 <viet.dan.1613@gmail.com>
2026-04-15 13:22:48 +0700

    update todo

--- src/features/to-do/index.tsx
+++ src/features/to-do/index.tsx
@@ -1,7 +1,16 @@
+// Build a Todo List application with the following requirements:
+
+// - Add a new todo(input + button)
+// - Mark a todo as completed / uncompleted
+// - Delete a todo
+// - Display total and completed count
+// - Prevent unnecessary re - renders(optimize performance)
+// - Store in localStorage
+
 export default function App() {
 	return (
 		<div>
-			Counter
+			Todo
 		</div>
 	)
 }
