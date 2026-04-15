ac9b9e79f614e1c87f584cf9ba7c6ae11983e57d
vietdan1613 <viet.dan.1613@gmail.com>
2026-04-15 13:27:22 +0700

    update fetch

--- src/features/fetch-data/index.tsx
+++ src/features/fetch-data/index.tsx
@@ -1,14 +1,21 @@
 
 // The Task
 //	- Fetch users from an API
-//	- Implement search by name
+//  	- https://jsonplaceholder.typicode.com/posts/1
+// 		{
+// 			id: 1,
+// 			title: '...',
+// 			body: '...',
+// 			userId: 1
+// 		}
+//	- Implement search by title
 //	- Avoid unnecessary re - renders
 //	- Keep the UI responsive
 
 export default function App() {
 	return (
 		<div>
-			App
+			Fetching
 		</div>
 	)
 }
