25867cbe40b878f5fe4296741d0cd560fc4af412
vietdan1613 <viet.dan.1613@gmail.com>
2026-04-16 09:10:37 +0700

    update

--- src/features/modal-component/index.tsx
+++ src/features/modal-component/index.tsx
@@ -1,7 +1,22 @@
+// Implement a reusable Modal component.
+
+// ✅ Requirements
+// 		- Open modal when clicking a button
+// 		- Close modal when:
+// 			- Clicking outside
+// 			- Pressing ESC
+// 		- Accept children content
+
+// 📌 Example usage
+// 		<button onClick={open}>Open Modal</button>
+// 		<Modal isOpen={isOpen} onClose={close}>
+// 			<h2>Hello</h2>
+//		</Modal>
+
 export default function App() {
 	return (
 		<div>
-			Counter
+			Modal
 		</div>
 	)
 }
